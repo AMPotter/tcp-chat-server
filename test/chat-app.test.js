d45231e75acf322d0c962d3deb55b466556d840c
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2018-07-09 17:53:29 -0700

    bingo

---
+++ test/chat-app.test.js
@@ -0,0 +1,92 @@
+const assert = require('assert');
+const createApp = require('../lib/chat-app');
+const net = require('net');
+
+describe('chat app server', () => {
+    const port = 15688;
+
+    let clients = null;
+    let server = null;
+    beforeEach(done => {
+        clients = [];
+        server = createApp();
+        server.listen(port, done);
+    });
+
+    afterEach(() => {
+        clients.forEach(client => client.destroy());
+        server.close();
+    });
+
+    function openClient(callback) {
+        let client = net.connect(port, () => {
+            client.setEncoding('utf8');
+            clients.push(client);
+            callback(null, client);
+        });
+    }
+
+    it('announces a client when they join', done => {
+        openClient((err, client) => {
+            client.once('data', data => {
+                assert.equal(data, 'user1: has joined\n');
+                done();
+            });
+        });
+    });
+
+    it('sends @all message to all users', done => {
+        openClient((err, client1) => {
+            openClient((err, client2) => {
+                client2.on('data', data => {
+                    assert.equal(data, [
+                        'user2: has joined\n',
+                        'user1: Hello world\n'
+                    ].join(''));
+                    done();
+                });
+                client1.write('@all Hello world');
+            });
+        });
+    });
+
+    it('changes username via @nick:newname', done => {
+        openClient((err, client1) => {
+            openClient((err, client2) => {
+                client2.once('data', data => {
+                    assert.equal(data, [
+                        'user2: has joined\n',
+                        'user1: changed name to cooluser\n',
+                        'cooluser: Hello world\n'
+                    ].join(''));
+                    done();
+                });
+                client1.write('@nick:cooluser');
+                client1.write('@all Hello world');
+            });
+        });
+    });
+
+    it('direct message does not broadcast', done => {
+        openClient((err, client1) => {
+            openClient((err, client2) => {
+                openClient((err, client3) => {
+                    client2.on('data', data => {
+                        if(data === 'user2: has joined\n') return;
+                        assert.ok(!/sekrit/.test(data), 'should not received');
+                    });
+
+                    client3.on('data', data => {
+                        assert.equal(data, [
+                            'user3: has joined\n',
+                            'user1: Hello sekrit\n'
+                        ].join(''));
+                        done();
+                    });
+
+                    client1.write('@dm:user3 Hello sekrit');
+                });
+            });
+        });
+    });
+});
---
+++ test/chat-room.test.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const ChatRoom = require('../lib/chat-room');
+
+describe('chat room', () => {
+    let chat = null;
+    let client = null;
+    beforeEach(() => {
+        chat = new ChatRoom();
+        client = {};
+        chat.addClient(client);
+    });
+
+    it('adds a client, assigns username to client', () => {
+        assert.ok(client.username);
+    });
+
+    it('clients can be retrieved via username', () => {
+        const got = chat.getClient(client.username);
+        assert.equal(got, client);
+    });
+
+    it('renames a client', () => {
+        const newUsername = 'custom';
+        const oldUsername = client.username;
+        const status = chat.renameClient(oldUsername, newUsername);
+
+        assert.equal(status, true);
+        assert.equal(client.username, newUsername);
+        assert.equal(chat.getClient(oldUsername), null);
+        assert.equal(chat.getClient(newUsername), client);
+    });
+
+    it('does nothing when renaming to existing username', () => {
+        const client2 = {};
+        chat.addClient(client2);
+        const oldUsername = client.username;
+        const status = chat.renameClient(oldUsername, client2.username);
+        
+        assert.equal(status, false);
+        assert.equal(client.username, oldUsername);
+        assert.equal(chat.getClient(client2.username), client2);
+        assert.equal(chat.getClient(client.username), client);
+    });
+
+    it('returns all clients', () => {
+        const client2 = {};
+        chat.addClient(client2);
+        const clients = chat.all();
+        assert.deepEqual(clients, [client, client2]);
+    });
+});
